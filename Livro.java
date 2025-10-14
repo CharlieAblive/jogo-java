968fdf32086b3a259fc59d8ac4b2ccd6b59d8779
CharlieAblive <charlieablive@gmail.com>
2025-10-14 23:17:30 +0000

    atividade, tentando criar os metodos

---
+++ Livro.java
@@ -0,0 +1,15 @@
+public class Livro {
+    String titulo;
+    String autor;
+    int anoPublicacao;
+    int quantPaginas;
+
+    public void calcularTempoLeitura() {
+        
+    }
+
+    public void exibirInfo() {
+
+
+    }
+}
