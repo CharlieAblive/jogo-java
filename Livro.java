9e6e60b8e5c9ba3a2d0c77fbcdde927cfdf9c345
CharlieAblive <charlieablive@gmail.com>
2025-10-15 00:51:39 +0000

    to tentando juro

--- Livro.java
+++ Livro.java
@@ -4,12 +4,58 @@ public class Livro {
     int anoPublicacao;
     int quantPaginas;
 
+    public Livro(String titulo, String autor, int anoPublicacao, int quantPaginas) {
+        this.titulo = "não definido";
+        this.autor = "não definido";
+        this.anoPublicacao = 0;
+        this.quantPaginas = 0;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+    public int getQuantPaginas() {
+        return quantPaginas;
+    }
+
+    public void setTitulo(String titulo){
+        if (titulo != null && !titulo.isEmpty()){
+            this.titulo = titulo;
+        } else {
+            System.out.println("Nome inválido.");
+        }
+    }
+    public void setAutor(String autor){
+        if (autor != null && !autor.isEmpty()){
+            this.autor = autor;
+        } else {
+            System.out.println("Nome inválido.");
+        }
+    }
+    public void setAnoPublicacao(int anoPublicacao){
+        this.anoPublicacao = anoPublicacao;
+    }
+    public void setQuantPaginas(int quantPaginas){
+        this.quantPaginas = quantPaginas;
+    }
+
     public void calcularTempoLeitura() {
-        
+        double tempoPagina = 0.2;
+        double tempoLeitura = quantPaginas * tempoPagina;
+        System.out.println("O tempo de leitura do livro " + titulo + " é " + tempoLeitura + " minutos.");
     }
 
     public void exibirInfo() {
-
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Publicado em: " + anoPublicacao);
+        System.out.println(quantPaginas + " páginas");
 
     }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Ola, git");
+        Livro livro1 = new Livro("Alice no país das maravilhas", "Lewis Carroll", 1865, 256);
+    
+    livro1.calcularTempoLeitura();
+    livro1.exibirInfo();
+    
     }
 }
